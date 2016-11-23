0279d1534759ae7ff8364307de55d4fa05e05c8b
tnn130130 <tnn130130@users.noreply.github.com>
2016-11-23 16:21:08 -0600

    Add files via upload

--- MIPS/Lexathon.asm
+++ MIPS/Lexathon.asm
@@ -11,27 +11,143 @@
 # https://docs.google.com/spreadsheets/d/1c5XmnOQwUe-ryxMq7yZ0FXpBPmGJkvcXKlpcZLSry6s/edit#gid=0
 	
 	.data
+#****************************************************************************************************	
 # Global Vars
 pNewLine: .asciiz "\n"
 gameTable:  .space 9 # space for 9 bytes: each byte is a character
 answer: .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 # zero is null terminator
+#****************************************************************************************************
 
+#****************************************************************************************************
 # PrintMenu Global Vars
 pPrintMenu1: .asciiz "Welcome to Lexathon!\n\n"
 pPrintMenu2: .asciiz "1) Start the game\n2) Instructions\n3) Exit\n"
+#****************************************************************************************************
 
+#****************************************************************************************************
 # printInstructions Global Vars	
 pPrintInstructions: .asciiz "Lexathon is a word game where you must find as many words\nof four or more letters in the alloted time\n\nEach word must contain the central letter exactly once,\nand other tiles can be used once\n\nYou start each game with 60 seconds\nFinding a new word increases time by 20 seconds\n\nThe game ends when:\n-Time runs out, or\n-You give up\n\nScores are determined by both the percentage of words found\nand how quickly words are found.\nso find as many words as quickly as possible.\n\n"	
+#****************************************************************************************************
 
+#****************************************************************************************************
 # randomizeBoard Global Vars
 vowels: .byte 'A', 'E', 'I', 'O', 'U'
+#****************************************************************************************************
 
+#****************************************************************************************************
 # printBoard Global Vars
 pPrintBoard1: .asciiz "| "
 pPrintBoard2: .asciiz " | "
+#****************************************************************************************************
+
+#****************************************************************************************************
+# startGame Global Vars
+pStartGamePrompt: .asciiz "Input: "
+pEnterWord: .asciiz "Enter word: "
+pPrintMenu3: .asciiz "\n1)Enter word \n2)Instructions \n3)Shuffle \n4)End game "
+pSeparator: .asciiz "\n---------------------------------------------------------------------------------\n"
+#***************************************************************************************************
+
+#***************************************************************************************************
+# loadDictionary Global Vars
+fin: .asciiz "TestDictionary.txt" #filename for input
+buffer: .space 8000 # initial storage of file (charcount+newlines+wordcount)
+bufferArray: .space 10000 # number of bytes in array (10 * english words)
+#***************************************************************************************************
+
+#***************************************************************************************************
+# checkDictionary Global Vars
+playerAnswer: .space 10
+pWordLength: .asciiz "\nWord length is: "
+pCheckWord: .asciiz "\nEnter a word to check: "
+pCheckingDictionary: .asciiz "\nChecking dictionary...\n"
+pWordFound: .asciiz "\nWord found!\n"
+pWordNotFound: .asciiz "\nWord not found...\n"
+pNotValid: .asciiz "\nAnswer not valid. Must be between 4-9 char.\n"
+#***************************************************************************************************
+
 	.text
 main:
+	jal loadDictionary
 	jal printMenu
+	j Exit
+#****************************************************************
+loadDictionary: #void loadDictionary()
+#**************
+# raises baseparam ($a0) to the power of exponentparam ($a1)
+#
+#
+# Register usage (after loading file)
+# $t0 - comparison
+# $t1 - wordCount // number of words out in array
+# $t2 - address of buffer current iterating
+# $t3 - current element of buffer
+# $t4 - for i vs 10 comparision
+# $t6 - current element of array (every word is 10 bytes apart)
+# t7  - i is current word length
+#**************
+
+#-----Open and Read file into buffer
+#-------------------------------------------------------------------
+# open file for reading
+li $v0, 13 # system call for open file
+la $a0, fin # input file name
+li $a1, 0 # open for reading (flags are 0: read, 1: write)
+li $a2, 0 # mode is ignored
+syscall # open a file (file descriptor return in $v0)
+move $s6, $v0 # save the file descriptor
+
+# read from file just opened
+li $v0, 14 # system call for read from file
+move $a0, $s6 # file descriptor
+la $a1, buffer # address of buffer from which to read
+li $a2, 8000 # hardcoded buffer length
+syscall # read from file
+
+# close the file
+li $v0, 16 # system call for close file
+move $a0, $s6 # file descriptor to close
+syscall # close file
+#------------------------------------------------------------------------
+
+
+#-----Load from buffer into array
+la $t2, buffer # address of buffer into $t2
+la $t6, bufferArray # address of buffer into $t6
+add $t1, $zero, $zero # wordCount = 0
+
+WordCountWhile: # Store words in array until wordCount is 28
+slti $t0, $t1, 1000 # t0 is 1 if wordCount < 1000
+bne $t0, 1, WordCountExit
+
+add $t7, $zero, $zero # current WordLength = 0
+WordElementWhile:# While(i <= 10)) - Loop through 10 bytes, setting array elements to buffer elts
+slti $t0, $t7, 9 # i < 11
+bne $t0, 1, WordElementExit
+lb $t3, ($t2) # $t3 has current element of buffer
+
+# If current char != newline char
+beq $t3, 10, WordElementIfExit # 10 is newLine char
+sb $t3, ($t6) # store current buffer elt into current array location
+
+addi $t2, $t2, 1 # only iterate buffer elt when \n is not the current char
+WordElementIfExit:
+addi $t7, $t7, 1 # iterate i
+addi $t6, $t6, 1 # iterate to next address of array - 
+		 # Array is iterating address even when buffer is not stay aligned by 10
+
+j WordElementWhile
+WordElementExit:
+
+addi $t2, $t2, 1 # iterate to next address of buffer to skip \n 
+addi $t6, $t6, 1 # iterate to next address of array b/c it is currently one behind due to i <
+addi $t1, $t1, 1 # increment wordCount - a dictionary worrd has been stored in bufferArray
+
+j WordCountWhile
+WordCountExit:
+
+#jr $ra
+#****************************************************************
 
 #****************************************************************
 printMenu: #void printMenu()
@@ -50,14 +166,14 @@ printMenu: #void printMenu()
 	syscall
 	
 	# Print pPrintMenu2 (second prompt)
-	addi $v0, $zero, 4 # Load "print string" SYSCALL service into revister $v0
+	addi $v0, $zero, 4 # Load "print string" SYSCALL service into register $v0
 	la $a0, pPrintMenu2 # Load argument value, to print, into $a0
 	syscall
 	
 	# Get input for choice into $v0
-	li $v0, 5 # Load "read integer" SYSCALL service into revister $v0
+	li $v0, 5 # Load "read integer" SYSCALL service into register $v0
 	syscall
-	add $t0, $v0, $zero # put choice into $t0
+	move $t0, $v0 # move input into temporary register
 	
 	# Print pPrintMenu2 (second prompt)
 	addi $v0, $zero, 4 # Load "print string" SYSCALL service into revister $v0
@@ -72,6 +188,7 @@ printMenuWhile:
 printMenuElse:	
 	bne $t0, 2, printMenuChoice # else if (choice == 2)
 	jal printInstructions #printInstructions();
+	jal pSeparator_ # Print separating line
 printMenuChoice:
 	# Print pPrintMenu2 (second prompt)
 	addi $v0, $zero, 4 # Load "print string" SYSCALL service into revister $v0
@@ -141,7 +258,7 @@ printBoardLoopEnd:
 	j printBoardLoop
 	
 printBoardLoopReturn:
-	# jr $ra
+	jr $ra
 #****************************************************************
 
 #****************************************************************
@@ -310,11 +427,285 @@ ShuffleBoardExit: jr $ra
 
 #****************************************************************
 
+#****************************************************************
+startGame: #void startGame()
+	#**************
+# When 1 is entered for player's choice 
+# this function is called to start the game
+# Sets up the board by randomizing it
+# Prints the board and loops to continue game
+
+# Register usage
+# $a0 -  holds player's decision and answer
+#**************
+
+	# Set up the board
+	jal randomizeBoard 
+	
+	startGameLoop:
+	#*****************************************************************************
+	# Print board
+	jal pNew_Line # print a newline char
+	
+	jal printBoard  # Print the board
+	
+	li $v0, 4 # Call "print string" syscall and print instructions
+	la $a0, pPrintMenu3
+	syscall
+	
+	jal pNew_Line # print a newline char
+
+	#*****************************************************************************
+	# Get user's answer
+	li $v0, 4 # "Call "print string" syscall and ask for user answer
+	la $a0, pStartGamePrompt
+	syscall
+	
+	li $v0, 5 # " Call "read integer" SYSCALL and ask for user choice
+	syscall
+	move $s2, $v0 # Save the answer into save register $s2
+	
+	jal pNew_Line # print a newline char
+	
+	li $t0, 1 # If player enters decision "1" go to checkDictionary()
+	beq $s2, $t0, goToCheckDictionary
+	
+	li $t0, 2 # If player enters decision "2" go to printInstructions()
+	beq $s2, $t0, goToPrintInstructions
+	
+	li $t0, 3 # If player enters decision "3" go to randomizeBoard()
+	beq $s2, $t0, goToRandomizeBoard
+	
+	li $t0, 4 # If player enters decision "4" end the game
+	beq $s2, $t0, endStartGameLoop
+	
+	j startGameLoop
+	
+	#************************************************************************************
+	# Instructions to run based on player's decision or 1 2 or 3
+	
+	# 1) Check the dictionary if a word is entered
+	goToCheckDictionary:
+		addu $sp, $sp, -4
+		sw $a0, 0($sp)
+		jal getPlayerAnswerLength
+		jal checkDictionary
+		lw $a0, 0($sp)
+		addu $sp, $sp 4
+		
+		j startGameLoop
+	
+	# 2) Print instructions
+	goToPrintInstructions: 
+		addu $sp, $sp, -4 # Save argument on stack 
+		sw $a0, 0($sp)
+		jal printInstructions
+		jal pSeparator_
+		lw $a0, 0($sp) # Restore argument
+		addu $sp, $sp, 4
+		
+		j startGameLoop
+	
+	# 3) Randomize board
+	goToRandomizeBoard:
+		addu $sp, $sp, -4 # Save argument on stack
+		sw $a0, 0($sp)
+		jal randomizeBoard
+		lw $a0, 0($sp)	# Restore argument
+		addu $sp, $sp, 4
+		
+		j startGameLoop 
+
+	# 4) Exit
+	endStartGameLoop:
+		j Exit
+#**********************************************************************************
+
+#******************************************************************
+getPlayerAnswerLength: # loop through player's answer to get length
+#******************************************************************
+# Register Usage:
+# $a0 - addrees of playerAnswer
+# $t1 - register for holding answer byte
+# $t2 - comparison
+#----------------------------------------------------------------------
+	
+	li $v0, 4 # SYSCALL 4: Print string; print the prompt
+	la $a0, pCheckWord
+	syscall
+	
+	li $v0, 8 # SYSCALL 8: Read string; read in user input for string
+	la $a0, playerAnswer # load input into playerAnswer
+	li $a1, 10 # maximum number of characters to read is 9
+	syscall
+	
+	la $a0, playerAnswer
+	li $s0, 0 # intialize index of playerAnswer[i] i = 0
+	getPlayerAnswerLengthLoop:
+
+		slti $t2, $s0, 10 # if i <= 9 run loop
+		bne $t2, 1, getPlayerAnswerLengthExit # if not, exit loop
+		
+		lb $t1, ($a0) # store byte of playerAnswer[i] in t1
+		beq $t1, 10, getPlayerAnswerLengthExit # if newline is encountered finish
+		beq $t1, $0, getPlayerAnswerLengthExit # if null character is encountered quit
+		
+		addi $s0, $s0, 1 # increment player's answer element
+		addi $a0, $a0, 1 # increment answer address
+
+	j getPlayerAnswerLengthLoop # go back through the loop
+	getPlayerAnswerLengthExit:
+		#addi $s0, $s0, -1 # compensate for null character
+		
+		li $v0, 4 # SYSCALL 4: Print string; print  'answer length is:' "
+		la $a0, pWordLength
+		syscall
+		
+		li $v0, 1 # SYSCALL 1: Print integer; print word length
+		la $a0, ($s0)
+		syscall
+		
+		slti $t0, $s0, 4
+		beq $t0, 1, notValidAnswer
+		
+		move $v0, $s0 # move length into return value
+		
+	jr $ra
+	
+	notValidAnswer:
+		li $v0, 4 # SYSCALL 4: Print String; answer must be 4-9 characters
+		la $a0, pNotValid
+		syscall
+		j getPlayerAnswerLength
+#****************************************************************
+
+#****************************************************************
+checkDictionary: # boolean checkDictionary()
+#************************************************************************************************************
+# Register usage (after loading file)
+# $a0 - address of playerAnswer
+# $a1 - address of dictionaryArray
+# $t3 - loaded address of playerAnswer and index
+# $t4 - loaded address of dictionaryArray
+# $s0 - length of player's answer
+# $t7 - number of letter matches
+# $t8 - dictionary word's length
+# $s6 - checkWordLoop counter
+# $s7 - checkDictionaryLoop count
+#-----------------------------------------------------------------------------------------------------
+# 0 1 2 3 4 5 6 7 8 9 <- dictionaryArray e.g.
+# _ _ _ _ _ _ _ _ _ /n empty chars are \0 'stupid'
+# 0 1 2 3 4 5 6 7 8 <- playerAnswer                                                                                                          012345 6 7 8
+# _ _ _ _ _ _ _ _ _ words less than total array size is padded with \n and then \0 if remaining placyes are empty e.g. inputting 'stupid' -> stupid\n\0\0
+#------loop through the dictionary; load player's answer element and dictionary letters and check to see if they match-------
+	
+	move $s0, $v0 # move argument from player answer length into register
+	
+	la $a0, playerAnswer # load address of player's answer 
+	la $a1, bufferArray # load address of dictionary array
+	
+	la $t4, ($a1) # load address of dictionary array; $t4 <- bufferArray address
+	
+	checkDictionaryLoop: # loop through the dictionary array until the end of the file is reached
+		beq $s0, $t7, wordAlmostFound # check if length of player's answer and dictionary word match
+		
+		la $t3, 0($a0) # reset playerAnswer[i] letter to beginning for rechecking of next word in dictionary
+		li $t7, 0 # reset number of letter matches to 0
+		li $s6, 0 # (A) counter for checkWordLoop
+		li $t8, 0 # (C) reset dictionary word length to 0
+		addi $s7, $s7, 1 # (B) increment checkDictionaryLoop count
+		beq $s7, 999, wordNotFound # (B) word not found if entire dictionary array was checked
+		
+		checkWordLoop:           #                       0 1 2 3 4 5 6 7 8 
+			slti $t0, $s6, 8 # (A) for(i <= 9) # run _ _ _ _ _ _ _ _ _ playerAnswer
+			bne $t0, 1, Skip #                        0 1 2 3 4 5
+					 #                        b a d a s s   b      a      d
+			lb $t5, ($t3) # load playerAnswer[i](A.1) e.g. _ _ _ _ _ _ 0($t5) 1($t5) 2($t5) and so on and so on...
+			lb $t6, ($t4) # load dictionaryArray[i] (A.2)
+			
+			addDictionaryWordLength: # (C) gets the length of the word in the dictionary
+				beq $t6, 0x0, addDictionaryWordLengthExit # if letters in the dictionary are newline or null skip 
+				beq $t6, 0xA, addDictionaryWordLengthExit # do not add to length else add to length
+				beq $t6, 0xD, addDictionaryWordLengthExit
+				addi $t8, $t8, 1 # if it is a letter add to the length
+			addDictionaryWordLengthExit:
+			
+			addi $t3, $t3, 1 # increment playerAnswer[i] refer to (A.1)
+			addi $t4, $t4, 1 # increment dictionaryArray[i] refer to (A.2)
+			addi $s6, $s6, 1 # (A) increment checkDictionaryCount
+			
+			beq $t5, $t6, letterMatches # compare the letters; if the letters match add 1 to number of matches
+			
+			j checkWordLoop # go back through loop
+			                                            #   d i c t i o n a r y word
+			Skip: # skip /n character in dictionaryArray[i] _ _ _ _ _ _ _ _ _ \n _
+				addi $t4, $t4, 2                    # * = pointer       * -> * skip 2 bytes     
+				j checkDictionaryLoop
+			
+			letterMatches:
+				beq $t5, 0x0, letterMatchesExit # if any of the characters are newLines or null skip letter match
+				beq $t6, 0x0, letterMatchesExit
+				beq $t5, 0xD, letterMatchesExit
+				beq $t6, 0xD, letterMatchesExit
+				addi $t7, $t7, 1
+				letterMatchesExit:
+				j checkWordLoop # if letter match check to see if next letter matches
+			
+
+				
+
+	j checkDictionaryExit
+#---------------------------------------------------------------------------------------------------------------
+	# Pass through one more filter
+	# If the length of the number of letter matches and length of dictionary word are equal THEN THE WORDS ARE THE SAME
+	wordAlmostFound:
+		beq $t7, $t8, wordFound
+		j wordNotFound
+#---------------------------------------------------------------------------------------------------------------
+	# If a word is found 
+	wordFound: 
+		li $v0, 4 # SYSCALL 4: print string; word was found
+		la $a0, pWordFound
+		syscall
+		
+		li $t0, 1 # return 1
+		move $v0, $t0
+		j checkDictionaryExit
+#--------------------------------------------------------------------------------------------------------------
+	# If no matches found 
+	wordNotFound: 
+		li $v0, 4 # SYSCALL 4: print string; word was not found
+		la $a0, pWordNotFound
+		syscall
+		
+		li $t0, 0 # return 0
+		move $v0, $t0
+		j checkDictionaryExit
+		
+#---------------------------------------------------------------------------------------------------------------
+	checkDictionaryExit:
+		jr $ra
+
+#---------------------------------------------------------------------------------------------------------------
+
+
+#******************************************************************************************************************
 
 # Dummy Functions:
-startGame:
-	j Exit
+# print newline
 
+pNew_Line:
+	li $v0, 4 # Call "print string" SYSCALL and print a new line
+	la $a0, pNewLine
+	syscall
+	jr $ra
+
+pSeparator_:
+	li $v0, 4 # Call "print string" SYSCALL and print a separator for formatting ----------------
+	la $a0, pSeparator
+	syscall
+	jr $ra
+	
 printInstructions:
 	# Print pPrintInstructions
 	addi $v0, $zero, 4 # Load "print string" SYSCALL service into revister $v0
